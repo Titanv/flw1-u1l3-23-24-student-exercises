843244318b453705ef8fb43b3b6bd82d238d0515
LRFlower <20254519-LRFlower@users.noreply.replit.com>
2023-10-18 22:24:57 +0000

    Modified CodeNation starter code to complete various tasks.

--- magicalCreature.js
+++ magicalCreature.js
@@ -2,25 +2,40 @@
 
 // 1. Declare a variable named 'creatureName' and assign it a magical creature's name as a string.
 
+let creatureName = "dragon";
 
 
 // 2. Declare a variable named 'creatureDescription' and describe its magical properties as a string.
 
+let creatureDescription = "huge, bat-winged, fire-breathing, scaly lizard or snake with a barbed tail.";
 
-// 3. Do you believe that this creature is friendly to humans? 
+// 3. Do you believe that this creature is friendl=y to humans? 
+//No
 // Declare a variable named 'isFriendly' and assign it a boolean value.
 
-
+let isFriendly = false;
 
 // 4. The magical zoo has an age restriction! Declare a variable named 'visitorAge' and assign it a random age between 1 and 60.
 // Hint: Use Math.floor() to round a number up.
 // Hint: Use Math.random() for generating random numbers.
 
+let a = Math.random() * 61;
+let visitorAge = Math.floor(a);
+
+console.log(visitorAge);
 
 
 // 5. Determine if the visitor is allowed to see the creature.
+//NO
 // Declare a variable named 'canVisit' and assign it an expression to see 
+let canVisit;
 // if the visitorAge variable is greater than 15.
+if (visitorAge > 15){
+  canVisit = true;
+}
+else{
+  canVisit = false;
+}
 // Hint: use this resource if you need support: https://www.w3schools.com/js/js_booleans.asp 
 
 
@@ -28,10 +43,13 @@
 // 6. Combine all the information into a summary.
 // Declare a variable named 'zooSummary' and combine all the above information using concatenation.
 
+let zooSummary = `Creature Name: ${creatureName} \nCreature Description: ${creatureDescription} \nIs friendly: ${isFriendly} \nCan Visit: ${canVisit} `
+
+// \n creates a new line for everything!
 
 // 7. Print the summary using console.log()
 
-
+console.log(zooSummary);
 
 // Challenge:
 // 8. Now, imagine there are three more creatures in the zoo. 
--- operators.js
+++ operators.js
@@ -3,26 +3,40 @@
 
 // 1. Addition
 // Create two variables, num1 and num2, and assign them any two numbers of your choice.
-// Add the two numbers and store the result in a new variable called sum.
-// Print the result using console.log().
+let num1 = 7;
+let num2 = 10;
 
+// Add the two numbers and store the result in a new variable called sum.
+let sum = num1 + num2;
+// Print the result using console.log().
+console.log(sum);
 
 
 // 2. Subtraction
 // Subtract num2 from num1 and store the result in a new variable called difference.
-// Print the result.
 
+let difference = num1 - num2;
+// Print the result.
+console.log(difference);
 
 // 3. Multiplication
 // Multiply num1 by num2 and store the result in a new variable called product.
-// Print the result.
 
+let product = num1 * num2;
+// Print the result.
+console.log(product);
 
 
 // String Operations
 
 // 4. Concatenation with +
 // Create two string variables, str1 and str2, and assign them any two strings of your choice.
+let str1 = "15a";
+let str2 = "15b";
 // Concatenate (combine) the two strings using the + operator and store the result in a new variable called combinedString.
 // Print the result.
+
+let combinedString = str1 + str2;
+
+console.log(combinedString);
 
